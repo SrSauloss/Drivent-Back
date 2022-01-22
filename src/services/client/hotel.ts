f9b8887ff7f4794ad485c2caf8accd9e9e853d01
SrSauloss <saulosilva@super.ufam.edu.br>
2022-01-22 16:27:13 -0400

    Added custom error

--- src/services/client/hotel.ts
+++ src/services/client/hotel.ts
@@ -1,4 +1,5 @@
 import Hotel from "@/entities/Hotel";
+import NotFoundError from "@/errors/NotFoundError";
 import HotelInformation from "@/interfaces/hotelInformation";
 
 export async function getAllHotels() {
@@ -13,5 +14,8 @@ export async function getAllHotels() {
 
 export async function getOneHotel(id: number) {
   const hotel = await Hotel.getSpecificHotelAndRooms(id);
+  if(!hotel) {
+    throw new NotFoundError();
+  }
   return hotel;
 }
