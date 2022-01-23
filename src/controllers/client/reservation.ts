e201753af062518b492eabdee8b9e04967d2830a
Audamon <joaotamagno@gmail.com>
2022-01-23 15:25:32 -0300

    No console

--- src/controllers/client/reservation.ts
+++ src/controllers/client/reservation.ts
@@ -1,17 +1,30 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 
 import ReservationData from "@/interfaces/reservation";
 import * as service from "@/services/client/reservation";
+import errorHandlingMiddleware from "@/middlewares/errorHandlingMiddleware";
 
-export async function createReservation(req: Request, res: Response) {
+export async function createReservation(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
   const reservationData = req.body as ReservationData;
   reservationData.userId = req.user.id;
-  const reservation = await service.createReservation(reservationData);
-  res.status(201).send(reservation);
+  try {
+    const reservation = await service.createReservation(reservationData);
+    res.status(201).send(reservation);
+  } catch (error) {
+    next(errorHandlingMiddleware);
+  }
 }
 
-export async function findReservation(req: Request, res: Response) {
+export async function findReservation(req: Request, res: Response, next: NextFunction) {
   const userId = req.user.id;
-  const reservation = await service.findReservation(userId);
-  res.status(200).send(reservation);
+  try {
+    const reservation = await service.findReservation(userId);
+    res.status(200).send(reservation);
+  } catch (error) {
+    next(errorHandlingMiddleware);
+  }
 }
--- src/controllers/client/tickets.ts
+++ src/controllers/client/tickets.ts
@@ -1,11 +1,20 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import TicketPrice from "@/entities/TicketPrice";
+import errorHandlingMiddleware from "@/middlewares/errorHandlingMiddleware";
 
-export async function getPrices(req: Request, res: Response) {
-  const prices = await TicketPrice.getPrices();
-  let pricesObj = {};
-  prices.forEach((price) => {
-    pricesObj = { ...pricesObj, [price.name]: price.price };
-  });
-  res.send(pricesObj);
+export async function getPrices(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const prices = await TicketPrice.getPrices();
+    let pricesObj = {};
+    prices.forEach((price) => {
+      pricesObj = { ...pricesObj, [price.name]: price.price };
+    });
+    res.send(pricesObj);
+  } catch (error) {
+    next(errorHandlingMiddleware);
+  }
 }
