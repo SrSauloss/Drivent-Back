2310582c71338e802daa4d0c3d49b33a9556e7b4
SrSauloss <saulosilva@super.ufam.edu.br>
2022-01-23 10:53:46 -0400

    Added authentication middleware

--- src/routers/client/hotel.ts
+++ src/routers/client/hotel.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import * as controller from "@/controllers/client/hotel";
+import authenticationMiddleware from "@/middlewares/tokenValidationMiddleware";
 
 const router = Router();
 
-router.get("/", controller.getAll);
-router.get("/:id", controller.getOne);
+router.get("/", authenticationMiddleware, controller.getAll);
+router.get("/:id", authenticationMiddleware, controller.getOne);
 
 export default router;
