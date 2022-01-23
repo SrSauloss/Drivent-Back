5a8b5de1b9d07daf127a511d39aa12cecfda6bdb
Audamon <joaotamagno@gmail.com>
2022-01-23 15:19:16 -0300

    No console

--- src/controllers/client/tickets.ts
+++ src/controllers/client/tickets.ts
@@ -2,12 +2,10 @@ import { Request, Response } from "express";
 import TicketPrice from "@/entities/TicketPrice";
 
 export async function getPrices(req: Request, res: Response) {
-  console.log(1);
   const prices = await TicketPrice.getPrices();
   let pricesObj = {};
   prices.forEach((price) => {
     pricesObj = { ...pricesObj, [price.name]: price.price };
   });
-  console.log(pricesObj);
   res.send(pricesObj);
 }
