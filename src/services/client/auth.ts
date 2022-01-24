ce823e5a1335f803496449c9c79c508914724162
Glauco Villas Boas <glappsmobile@gmail.com>
2022-01-23 21:47:35 -0300

    disabled eslint no-unused-vars for session

--- src/services/client/auth.ts
+++ src/services/client/auth.ts
@@ -15,6 +15,7 @@ export async function signIn(email: string, password: string) {
     userId: user.id
   }, process.env.JWT_SECRET);
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const session = await Session.createNew(user.id, token);
 
   return {
