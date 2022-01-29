52b43e39a9d0d04a14980044c73aefbea8585b20
Glauco Villas Boas <glappsmobile@gmail.com>
2022-01-29 15:57:38 -0300

    created tests for route GET /activities

--- tests/integration/activity.test.ts
+++ tests/integration/activity.test.ts
@@ -20,15 +20,41 @@ afterAll(async() => {
 });
 
 describe("GET /activities/dates", () => {
-  it("Should return 401 when user gives an invalid token", async() => {
+  it("Should return 401 when token is invalid", async() => {
     const result = await supertest(app).get("/activities/dates");
     expect(result.status).toEqual(401);
   });
 
-  it("Should return 200 and an array of dates when user gives a valid token", async() => {
+  it("Should return 200 and an array of dates when token is valid", async() => {
     const { token } = await createSession();
     const result = await supertest(app).get("/activities/dates").set("Authorization", `Bearer ${token}`);
     expect(result.status).toEqual(200);
     expect(Array.isArray(result.body)).toBe(true);
+  });
+});
+
+describe("GET /activities", () => {
+  it("Should return 401 when token is invalid", async() => {
+    const result = await supertest(app).get("/activities");
+    expect(result.status).toEqual(401);
+  });
+
+  it("Should return 200 and an array of activities the parameter date is valid", async() => {
+    const { token } = await createSession();
+    const result = await supertest(app).get("/activities?date=2022-05-05").set("Authorization", `Bearer ${token}`);
+    expect(result.status).toEqual(200);
+    expect(Array.isArray(result.body)).toBe(true);
+  });
+
+  it("Should return 422 when no date is given", async() => {
+    const { token } = await createSession();
+    const result = await supertest(app).get("/activities").set("Authorization", `Bearer ${token}`);
+    expect(result.status).toEqual(422);
+  });
+
+  it("Should return 422 when date is invalid", async() => {
+    const { token } = await createSession();
+    const result = await supertest(app).get("/activities?05-05-2022").set("Authorization", `Bearer ${token}`);
+    expect(result.status).toEqual(422);
   });
 });
