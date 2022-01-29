ae7513bd0eae8fee064fdf1166aa5311c8b8ac05
Glauco Villas Boas <glappsmobile@gmail.com>
2022-01-29 16:17:25 -0300

    removed useless console

--- src/middlewares/schemaValidatingMiddleware.ts
+++ src/middlewares/schemaValidatingMiddleware.ts
@@ -9,7 +9,6 @@ interface ValidationOptions {
 
 export default function schemaValidatingMiddleware(schema: Schema, options: ValidationOptions = { abortEarly: false }, checkQuery = false) {
   return function(req: Request, res: Response, next: NextFunction) {
-    console.log("teste");
     const object = checkQuery? req.query : req.body;
     const validation = schema.validate(object, { abortEarly: options.abortEarly });
 
